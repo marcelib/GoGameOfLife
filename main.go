07adcc7100afdd46fca921553fae09d4396ef958
Marcelele <marceli.baczewski@gmail.com>
2017-07-26 22:11:34 +0200

    Add Vue.js front-end webpack configuration.

--- main.go
+++ main.go
@@ -1,24 +1,20 @@
-package main
+package GoLife
+
+import "context"
+
 import (
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
+	"github.com/marcelib/iris"
+	"github.com/marcelib/iris/context"
 )
+
 func main() {
 	app := iris.New()
-	// Load all templates from the "./templates" folder
-	// where extension is ".html" and parse them
-	// using the standard `html/template` package.
-	app.RegisterView(iris.HTML("./templates", ".html"))
+	app.RegisterView(iris.HTML("../app", ".html"))
 
-	// Method:    GET
-	// Resource:  http://localhost:8080
 	app.Get("/", func(ctx context.Context) {
-		// Bind: {{.message}} with "Hello world!"
 		ctx.ViewData("message", "Hello world!")
-		// Render template file: ./templates/hello.html
 		ctx.View("hello.html")
 	})
 
-	// Start the server using a network address and block.
 	app.Run(iris.Addr(":8080"))
 }
